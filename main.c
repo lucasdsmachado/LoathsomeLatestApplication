2c5cf7aafefbf84c806f4abd982282c5eabf2266
lucasdsmachado <briandsmachado@gmail.com>
2023-05-18 02:16:02 -0300

    style: apply consistent formatting to multiple functions

--- main.c
+++ main.c
@@ -2,49 +2,38 @@
 #include <stdlib.h>
 #include "record.h"
 
-void initializeFile(FILE *f, int m)
-{
+void initializeFile(FILE* f, int m) {
   printf("Initializing \"database.dat\" file...\n");
 
   Record r;
   r.occupied = false;
   r.ptr = -1;
-  for (int i = 0; i < m; i++)
-  {
+  for (int i = 0; i < m; i++) {
     r.pos = i;
     fwrite(&r, sizeof(Record), 1, f);
   }
 }
 
-void readRecords(FILE *f, int m)
-{
+void readRecords(FILE* f, int m) {
   Record r;
   fseek(f, 0, SEEK_SET);
   printf("%-15s%-15s%-15s\n", "Addr", "Reg", "Ptr");
-  for (int i = 0; i < m; i++)
-  {
+  for (int i = 0; i < m; i++) {
     fread(&r, sizeof(Record), 1, f);
-    if (r.occupied)
-    {
+    if (r.occupied) {
       printf("%-15d%-15d", r.pos, r.value);
-      if (r.ptr != -1)
-      {
+      if (r.ptr != -1) {
         printf("%-15d\n", r.ptr);
-      }
-      else
-      {
+      } else {
         printf("%-15s\n", "null");
       }
-    }
-    else
-    {
+    } else {
       printf("%-15d%-15s%-15s\n", r.pos, "null", "null");
     }
   }
 }
 
-void insertRecord(FILE *f, int pos, int val, int ptr)
-{
+void insertRecord(FILE* f, int pos, int val, int ptr) {
   Record r;
   r.occupied = true;
   r.pos = pos;
@@ -53,26 +42,21 @@ void insertRecord(FILE *f, int pos, int val, int ptr)
 
   // printf("Storing record in file ...\n");
   fseek(f, pos * sizeof(Record), SEEK_SET);
-  if (fwrite(&r, sizeof(Record), 1, f) == 1)
-  {
+  if (fwrite(&r, sizeof(Record), 1, f) == 1) {
     // printf("Record stored successfully\n");
-  }
-  else
-  {
+  } else {
     // printf("Error storing the record\n");
   }
 }
 
-bool checkRecord(FILE *f, int pos)
-{
+bool checkRecord(FILE* f, int pos) {
   Record r;
   fseek(f, pos * sizeof(Record), SEEK_SET);
   fread(&r, sizeof(Record), 1, f);
   return r.occupied == true;
 }
 
-int getNextIntFromFile(FILE *d)
-{
+int getNextIntFromFile(FILE* d) {
   char line[100];
   int m;
   fgets(line, 100, d);
@@ -80,82 +64,66 @@ int getNextIntFromFile(FILE *d)
   return m;
 }
 
-void linearProbing(FILE *f, FILE *d, int m)
-{
-  while (!(feof(d)))
-  {
+void linearProbing(FILE* f, FILE* d, int m) {
+  while (!(feof(d))) {
     int n = getNextIntFromFile(d);
     int h = n % m;
-    while (checkRecord(f, h))
-    {
+    while (checkRecord(f, h)) {
       h = h + 1 > m - 1 ? h - m : h + 1;
     }
     insertRecord(f, h, n, -1);
   }
 }
 
-void doubleHashing(FILE *f, FILE *d, int m)
-{
-  while (!(feof(d)))
-  {
+void doubleHashing(FILE* f, FILE* d, int m) {
+  while (!(feof(d))) {
     int n = getNextIntFromFile(d);
     int h = n % m;
     int j = n >= m ? n / m : 1;
-    while (checkRecord(f, h))
-    {
+    while (checkRecord(f, h)) {
       h = h + j > m - 1 ? h + j - m : h + j;
     }
     insertRecord(f, h, n, -1);
   }
 }
 
-int findEmptyPosition(FILE *f, int m)
-{
+int findEmptyPosition(FILE* f, int m) {
   int pos = m - 1;
-  while (checkRecord(f, pos))
-  {
+  while (checkRecord(f, pos)) {
     pos -= 1;
   }
   return pos;
 }
 
-Record getRecord(FILE *f, int pos)
-{
+Record getRecord(FILE* f, int pos) {
   Record r;
   fseek(f, pos * sizeof(Record), SEEK_SET);
   fread(&r, sizeof(Record), 1, f);
   return r;
 }
 
-Record getLastRecord(FILE *f, int pos)
-{
+Record getLastRecord(FILE* f, int pos) {
   Record r = getRecord(f, pos);
-  while (r.ptr != -1)
-  {
+  while (r.ptr != -1) {
     r = getRecord(f, r.ptr);
   }
   return r;
 }
 
-Record getRecordWithPtr(FILE *f, int pos, int ptr)
-{
+Record getRecordWithPtr(FILE* f, int pos, int ptr) {
   Record r;
   r = getRecord(f, pos);
-  while (r.ptr != ptr)
-  {
+  while (r.ptr != ptr) {
     r = getRecord(f, r.ptr);
   }
   return r;
 }
 
-void explicitChaining(FILE *f, FILE *d, int m)
-{
-  while (!(feof(d)))
-  {
+void explicitChaining(FILE* f, FILE* d, int m) {
+  while (!(feof(d))) {
     int n = getNextIntFromFile(d);
     int h = n % m;
-    if (checkRecord(f, h))
-    {
+    if (checkRecord(f, h)) {
       Record r = getLastRecord(f, h);
       int ptr = findEmptyPosition(f, m);
       insertRecord(f, r.pos, r.value, ptr);
@@ -165,24 +133,18 @@ void explicitChaining(FILE *f, FILE *d, int m)
   }
 }
 
-void explicitChainingNoJoining(FILE *f, FILE *d, int m)
-{
-  while (!(feof(d)))
-  {
+void explicitChainingNoJoining(FILE* f, FILE* d, int m) {
+  while (!(feof(d))) {
     int n = getNextIntFromFile(d);
     int h = n % m;
-    if (checkRecord(f, h))
-    {
+    if (checkRecord(f, h)) {
       Record r = getRecord(f, h);
       int ptr = findEmptyPosition(f, m);
-      if (r.value % m == r.pos)
-      {
+      if (r.value % m == r.pos) {
         r = getLastRecord(f, h);
         insertRecord(f, r.pos, r.value, ptr);
         h = ptr;
-      }
-      else
-      {
+      } else {
         Record p = getRecordWithPtr(f, r.value % m, r.pos);
         insertRecord(f, p.pos, p.value, ptr);
         insertRecord(f, ptr, r.value, r.ptr);
@@ -192,11 +154,9 @@ void explicitChainingNoJoining(FILE *f, FILE *d, int m)
   }
 }
 
-int countIntegersInFile(FILE *d)
-{
+int countIntegersInFile(FILE* d) {
   int total = 0;
-  while (!(feof(d)))
-  {
+  while (!(feof(d))) {
     getNextIntFromFile(d);
     total++;
   }
@@ -204,12 +164,10 @@ int countIntegersInFile(FILE *d)
   return total - 1;
 }
 
-int computeOpenAddressingAccessCount(FILE *f, int j, int m, int n)
-{
+int computeOpenAddressingAccessCount(FILE* f, int j, int m, int n) {
   Record r;
   int accesses = 0, h = n % m;
-  do
-  {
+  do {
     r = getRecord(f, h);
     h = h + j > m - 1 ? h + j - m : h + j;
     accesses++;
@@ -217,20 +175,15 @@ int computeOpenAddressingAccessCount(FILE *f, int j, int m, int n)
   return accesses;
 }
 
-int evaluateOpenAddressingTotalAccesses(FILE *f, FILE *d, char type)
-{
+int evaluateOpenAddressingTotalAccesses(FILE* f, FILE* d, char type) {
   rewind(d);
   int sum = 0, m = getNextIntFromFile(d);
-  while (!(feof(d)))
-  {
+  while (!(feof(d))) {
     int n = getNextIntFromFile(d);
     int j;
-    if (type == 'd')
-    {
+    if (type == 'd') {
       j = n >= m ? n / m : 1;
-    }
-    else
-    {
+    } else {
       j = 1;
     }
     sum += computeOpenAddressingAccessCount(f, j, m, n);
@@ -238,12 +191,10 @@ int evaluateOpenAddressingTotalAccesses(FILE *f, FILE *d, char type)
   return sum;
 }
 
-int computeChainedHashingAccessCount(FILE *f, int m, int n)
-{
+int computeChainedHashingAccessCount(FILE* f, int m, int n) {
   Record r;
   int accesses = 0, h = n % m;
-  do
-  {
+  do {
     r = getRecord(f, h);
     h = r.ptr;
     accesses++;
@@ -251,29 +202,23 @@ int computeChainedHashingAccessCount(FILE *f, int m, int n)
   return accesses;
 }
 
-int evaluateChainedHashingTotalAccesses(FILE *f, FILE *d)
-{
+int evaluateChainedHashingTotalAccesses(FILE* f, FILE* d) {
   rewind(d);
   int sum = 0, m = getNextIntFromFile(d);
-  while (!(feof(d)))
-  {
+  while (!(feof(d))) {
     int n = getNextIntFromFile(d);
     sum += computeChainedHashingAccessCount(f, m, n);
   }
   return sum;
 }
 
-int main()
-{
+int main() {
   FILE *f, *d;
 
   printf("Acessing \"database.dat\" and \"file.txt\"...\n");
-  if ((f = fopen("database.dat", "r+")) && (d = fopen("file.txt", "r+")))
-  {
+  if ((f = fopen("database.dat", "r+")) && (d = fopen("file.txt", "r+"))) {
     printf("Files accessed successfully.\n");
-  }
-  else
-  {
+  } else {
     printf("Error accessing files.\n");
     exit(-1);
   }
@@ -281,12 +226,12 @@ int main()
   int n = countIntegersInFile(d);
   int m = getNextIntFromFile(d);
 
-  initializeFile(f, m); // cria arquivo com m registros
+  initializeFile(f, m);  // cria arquivo com m registros
   // linearProbing(f, d, m);
   // doubleHashing(f, d, m);
   // explicitChaining(f, d, m);
   explicitChainingNoJoining(f, d, m);
-  readRecords(f, m); // printa os m registos
+  readRecords(f, m);  // printa os m registos
 
   fclose(f);
   fclose(d);
